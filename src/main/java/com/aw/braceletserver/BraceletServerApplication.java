321386532862b74e139c84430d460c60c3602435
xusisheng <xusisheng@hotmail.com>
2020-04-13 08:48:19 +0800

    add

---
+++ src/main/java/com/aw/braceletserver/BraceletServerApplication.java
@@ -0,0 +1,13 @@
+package com.aw.braceletserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraceletServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraceletServerApplication.class, args);
+    }
+
+}
